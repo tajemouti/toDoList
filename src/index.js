94ba4d08a4b89e70e1c435b5745d94975c5ff9d1
tajemouti <tajemouti@gmail.com>
2023-07-28 18:55:33 +0100

    Refactor code for better maintainability

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+import MyTask from './crud.js';
 import './style.css';
-import displayList from './crud.js';
-import { barDone, clearDone } from './cleardone.js';
 
-const taskList = document.getElementById('tasks');
-taskList.addEventListener('click', barDone);
+const taskInput = document.getElementById('taskInput');
+const enterBtn = document.getElementById('enter');
+const refreshBtn = document.getElementById('refresh');
 
-const clearBtn = document.getElementById('clear');
-clearBtn.addEventListener('click', () => {
-  clearDone();
-  displayList();
+MyTask.displayList();
+
+taskInput.addEventListener('click', (e) => {
+  e.preventDefault();
 });
 
-displayList();
+enterBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (taskInput.value) {
+    const description = taskInput.value;
+    const done = new MyTask(description);
+    done.addTask();
+  }
+  return 0;
+});
+
+document.querySelectorAll('#clear').forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    MyTask.clearCompleted();
+  });
+});
+
+refreshBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  MyTask.refresh();
+});
